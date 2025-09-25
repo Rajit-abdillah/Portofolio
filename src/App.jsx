5cfae2276ab65d556bd99ded4951c8eea59c5418
Rajit-abdillah <abdillahrajit@gmail.com>
2025-09-25 07:05:27 +0700

    added routing for shooter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Navbar, Coldplatestudy, WeatherApp, Whatamovie } from "./components/index";
+import { Navbar, Coldplatestudy, WeatherApp, Whatamovie, Shooterstudy } from "./components/index";
 import Home from "./pages/home.jsx";
 import Footer from "./components/footer.jsx";
 
@@ -15,6 +15,7 @@ function App() {
         <Route path="/coldplate-study" element={<Coldplatestudy />} />
         <Route path="/weather-study" element={<WeatherApp />} />
         <Route path="/whatamovie-study" element={<Whatamovie/>} />
+        <Route path="/shooter-study" element={<Shooterstudy />} />
       </Routes>
       <Footer />
     </Router>

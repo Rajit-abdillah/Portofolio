1bb9f5a89a57281164171da41cea4c86ba45579c
Rajit-abdillah <abdillahrajit@gmail.com>
2025-09-25 07:05:40 +0700

    added import for shooter image

--- src/assets/asset.js
+++ src/assets/asset.js
@@ -1,3 +1,4 @@
 export {default as WeatherImage} from '../assets/weather-main.png'
 export {default as ColdplateImage} from '../assets/coldplate-main.png'
 export {default as Whatamovie} from '../assets/whatamovie.png'
+export {default as Shooter} from '../assets/shoother-main.png'
